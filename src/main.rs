a26196e73032d43072f21237a8629f96fc7d382f
Hyeon Kim <simnalamburt@gmail.com>
2023-06-12 02:10:04 +0900

    Render first frame

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use libavif_sys as c;
+use minifb::{Window, WindowOptions};
 
 fn main() {
-    unsafe { avif() }
+    unsafe { unsafe_main() }
 }
 
-unsafe fn avif() {
+unsafe fn unsafe_main() {
+    //
+    // Decode AVIF
+    //
     let avif_bytes = include_bytes!("../assets/frog.avif");
 
     let decoder = c::avifDecoderCreate();
@@ -18,29 +22,26 @@ unsafe fn avif() {
         err => panic!("avifDecoderParse() failed: {err}"),
     }
 
-    let width = (*(*decoder).image).width;
-    let height = (*(*decoder).image).height;
+    let width = (*(*decoder).image).width as usize;
+    let height = (*(*decoder).image).height as usize;
     let mut frames = Vec::with_capacity((*decoder).imageCount as usize);
 
     // Parse frames loop
     loop {
-        let mut buffer = vec![0u8; 4 * width as usize * height as usize];
-
         match c::avifDecoderNextImage(decoder) {
             c::AVIF_RESULT_OK => (),
             c::AVIF_RESULT_NO_IMAGES_REMAINING => break,
             err => panic!("avifDecoderNextImage() failed: {err}"),
         }
 
-        let mut rgb = c::avifRGBImage {
-            ..Default::default()
-        };
+        let mut rgb = c::avifRGBImage::default();
         let p_rgb = &mut rgb as *mut c::avifRGBImage;
 
         c::avifRGBImageSetDefaults(p_rgb, (*decoder).image);
         rgb.format = c::AVIF_RGB_FORMAT_RGBA;
         rgb.depth = 8;
-        rgb.rowBytes = 4 * width;
+        rgb.rowBytes = 4 * width as u32;
+        let mut buffer = vec![0u8; 4 * width * height];
         rgb.pixels = buffer.as_mut_ptr();
 
         match c::avifImageYUVToRGB((*decoder).image, p_rgb) {
@@ -51,6 +52,22 @@ unsafe fn avif() {
         frames.push(buffer);
     }
     assert_eq!(frames.len(), (*decoder).imageCount as usize);
-
     c::avifDecoderDestroy(decoder);
+
+    //
+    // Windowing
+    //
+    let mut window = Window::new("debug-avif", width, height, WindowOptions::default()).unwrap();
+    while window.is_open() {
+        let buf: Vec<_> = frames[0]
+            .chunks(4)
+            .map(|rgba| {
+                let [r, g, b, _] = *rgba else { unreachable!() };
+                // TODO: Treat alpha properly
+                (r as u32) << 16 | (g as u32) << 8 | (b as u32)
+            })
+            .collect();
+
+        window.update_with_buffer(&buf, width, height).unwrap();
+    }
 }
